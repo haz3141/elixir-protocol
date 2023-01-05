4830916a546a326cdf3086bf22e15998c1ae24ae
haz3141 <38766403+haz3141@users.noreply.github.com>
2023-01-04 19:40:28 -0500

    Updated title

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,7 @@
 export const AppConfig = {
   site_name: 'Elixir Protocol',
-  title: 'Elixir Protocol',
+  title:
+    'Elixir Protocol | Waste-to-Energy Protocol for Feeding & Empowering Developing Communities',
   description:
     'Waste-to-Energy Protocol for Feeding & Empowering Developing Communities',
   locale: 'en',
